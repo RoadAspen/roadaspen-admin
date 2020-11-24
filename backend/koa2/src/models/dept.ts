0ad4f2edbd110626d0fb4bab763f9bfc84f3fd4e
RoadAspen <709208680@qq.com>
2020-11-24 17:21:40 +0800

    add menu

--- backend/koa2/src/models/dept.ts
+++ backend/koa2/src/models/dept.ts
@@ -1,10 +1,10 @@
 /**
- *  menu 菜单管理
+ * dept 部门 表
  */
 
 import { Model, model, Document, Schema } from "mongoose";
 
-export interface IMenu {
+export interface IDept {
     component: string; // 组件路径
     createBy: string | null; // 创建者id
     createTime: string | null; //创建时间
@@ -19,29 +19,29 @@ export interface IMenu {
     path: string; // 路由
     perms: string; // 权限编码
     remark: string | null; // 备注
-    status: 0 | 1; // 状态 0 停用 1 正常
+    status: number; // 状态 0 停用 1 正常
     updateBy: string | null; // 更新者
     updateTime: string | null; // 更新时间
   }
-export interface IMenuModel extends IMenu, Document {}
+export interface IDeptModel extends IDept, Document {}
 
-const MenuSchema:Schema  = new Schema({
-    menuName:{type: String}, // 菜单名称
-    menuType:{type: String}, // 菜单类型
-    menuOrder:{type: Number}, // 显示排序
-    parent:{type:String}, // 父菜单名称
-    path:{type: String}, // 路由
+const DeptSchema:Schema  = new Schema({
+    menuName:{type: String,required:true}, // 菜单名称
+    menuType:{type: String,required:true}, // 菜单类型
+    menuOrder:{type: Number,required:true}, // 显示排序
+    parent:{type:Schema.Types.ObjectId,required:true,ref:'Menu'}, // 父菜单id
+    path:{type: String,required:true}, // 路由
     perms:{type: String}, // 权限编码
     component:{type: String}, // 组件路径
-    createBy:{type: String}, // 创建者id
+    createBy:{type: Schema.Types.ObjectId,ref:'User'}, // 创建者 外链至 User表
     createTime:{type: String}, //创建时间
     icon:{type: String}, // 图标
     isCache:{type: Boolean}, // 是否缓存
     isFrame:{type: Boolean}, // 是否外链
     remark:{type: String}, // 备注
-    status:{type:Number,default:1}, // 状态 0 停用 1 正常
+    status:{type: Number,default:1}, // 状态 0 停用 1 正常
     updateBy:{type: String}, // 更新者
     updateTime:{type: String}, // 更新时间
 });
 
-export const Menu:Model<IMenuModel> = model<IMenuModel>("menu",MenuSchema,"menu");
+export const Dept:Model<IDeptModel> = model<IDeptModel>("dept",DeptSchema,"dept");
--- backend/koa2/src/models/user.ts
+++ backend/koa2/src/models/user.ts
@@ -4,16 +4,37 @@
 
 import { Model, model, Document, Schema } from "mongoose";
 
-import { IUser } from "../interfaces/system";
+export interface IUser {
+    admin: boolean; // 是否是admin用户
+    userName: string; // 用户名
+    password:string; // 密码
+    createBy: string; // 创建者
+    createTime: string; // 创建时间
+    updateBy: string; // 更新者
+    updateTime: string; // 更新时间
+    email: string; // 邮箱
+    loginDate: string; //登录时间
+    loginIp: string; // 登录IP
+    nickName: string; // 用户昵称
+    phoneNumber: string; // 手机号
+    remark: string; // 备注
+    roleIds: string; // 角色ID，逗号分隔
+    roles: string[]; // 角色id
+    sex: string; // 性别
+    status: string; // 账号状态 1 正常 || 0 停用
+    description: string; // 角色描述
+    order: number; // 显示顺序, 排序
+  }
 
 export interface IUserModel extends IUser, Document {}
+
+
 
 const UserSchema:Schema  = new Schema({
     admin:{type:Boolean,default:false}, // 是否是admin用户
     userName:{type:String,unique:true,required:true},// 用户名
     password:{type:String,required:true}, // 用户密码
-    userId:{type:Number,unique:true},// 用户id 默认唯一
-    createBy:{type:String,default:null}, // 创建者
+    createBy:{type:Schema.Types.ObjectId,ref:'User'}, // 创建者
     createTime:{type:Date,default:Date.now}, // 创建时间
     email:{type:String}, // 邮箱
     loginDate:{type:String}, //登录时间
@@ -21,10 +42,9 @@ const UserSchema:Schema  = new Schema({
     nickName:{type:String}, // 用户昵称
     phoneNumber:{type:String},// 手机号 
     remark:{type:String}, // 备注
-    roleIds:{type:String,default:null},// 角色ID，逗号分隔
-    roles:{type:Array,default:[]},// 角色id
+    roles:[{type:Schema.Types.ObjectId,ref:'Role'}],// 角色id
     sex:{type:String}, // 性别
-    status:{type:String,default:0},// 账号状态 0 正常 || 1 停用
+    status:{type:String,default:1},// 账号状态 1 正常 || 0 停用
     description:{type:String}, // 角色描述
     order:{type:Number,default:0}, // 显示顺序, 排序
     updateBy:{type:String},// 更新者
